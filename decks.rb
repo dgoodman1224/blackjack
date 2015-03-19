92b0edf32ccd9c2b2a3849c176dfcbd2c54fdaf9
David Goodman <david@myeatclub.com>
2015-03-19 08:58:58 -0700

    Uncomment high cards

--- decks.rb
+++ decks.rb
@@ -44,7 +44,7 @@ class Deck
 
 	def create_standard_deck
 		card_values = {'Two' => 2,'Three' => 3,'Four' => 4,'Five' => 5,'Six' => 6,#'Seven' => 7,'Eight' => 8,
-						'Nine' => 9,#'Ten' => 10,'Jack' => 10,'Queen' => 10,'King' => 10,
+						'Nine' => 9,'Ten' => 10,'Jack' => 10,'Queen' => 10,'King' => 10,
 						'Ace' => 11}
 		suits = ['S', 'D', 'C', 'H']
 		suits.each do |suit|
